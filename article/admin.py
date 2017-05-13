d37a9e4941b35d87fab860bb71a581f8393154fd
hehuijun <he.huijun@gmail.com>
2017-05-14 01:10:06 +0800

    第一个djang web程序
    继续完善

---
+++ article/admin.py
@@ -0,0 +1,13 @@
+from django.contrib import admin
+from article.models import Article
+#from article.models import ArticleAdmin
+
+
+
+
+
+
+# Register your models here.
+admin.site.register(Article)
+
+#admin.site.register(ArticleAdmin)
